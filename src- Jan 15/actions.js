66c551d3d95ca0ad2db08e0160c3f53bc395a94a
Nikhil Raj <44263051+nikhilraj01@users.noreply.github.com>
2021-01-21 13:56:27 +0530

    icon updated

--- src- Jan 15/actions.js
+++
@@ -1,6 +0,0 @@
-const actions = {
-  SELECT_VESSEL: "SELECT_VESSEL",
-  ADD_CONTAINER: "ADD_CONTAINER",
-};
-
-export default actions;
--- src- Jan 15/reducers/index.js
+++
@@ -1,6 +0,0 @@
-import { combineReducers } from "redux";
-import pageReducer from "./pageReducer";
-
-const rootReducer = combineReducers({ pageReducer });
-
-export default rootReducer;
